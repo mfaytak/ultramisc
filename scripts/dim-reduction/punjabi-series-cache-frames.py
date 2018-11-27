e0e875e143dd5e7ca9f8333d2ea872c9c811b8c4
Matthew Faytak <mf@berkeley.edu>
2018-11-26 19:52:04 -0800

    Add punjabi caching, image proc

--- scripts/dim-reduction/punjabi-series-cache-frames.py
+++ scripts/dim-reduction/punjabi-series-cache-frames.py
@@ -37,53 +37,92 @@ data = None
 recs = []
 frames_out = os.path.join(expdir,"frames.npy")
 metadata_out = os.path.join(expdir,"frames_metadata.pickle")
-png_glob_exp = os.path.join(os.path.normpath(expdir),"*","*","*.png")
 
 # for filename in list-of-files:
-for frame in glob.glob(png_glob_exp): 
+for path, dirs, files in os.walk(expdir):
+    for d in dirs:
+        png_glob_exp = os.path.join(path, d, "*.png")
+        glob_iter = glob.glob(png_glob_exp)
+        if len(glob_iter) > 0: # if there are any .png files in directory
+            idx_list = []
+            for png in glob_iter:
+                # retrieve and sort frame indices from file path strings
+                fr_num = re.search('_(\d+).png$', png)
+                #print(png, fr_num.group(1))
+                idx_list.append(int(fr_num.group(1)))
+            start_idx = min(idx_list)
+            end_idx = max(idx_list)
+            #print(start_idx,end_idx)
+            #print(idx_list)
+            mid_idx = int(np.floor((start_idx + end_idx)/2))
+            try:
+                assert mid_idx in idx_list
+            except AssertionError:
+                print("Desired mid frame index {:} isn't available.".format(mid_idx))
+                sys.exit(2)
+                
+            # get paths for evenly spaced frames between 0th and midpoint frame
+            series_frames = []
+            for idx in np.linspace(start_idx, mid_idx, num=4):
+                idx_int = int(np.floor(idx))
+                #rint(idx_int)
+                glob_finder = glob.glob(os.path.join(path, d, "*_{:}.png".format(idx_int)))
+                if len(glob_finder) == 1:
+                    try:
+                        # get file path and try to grab some metadata from it
+                        frame_path = glob_finder[0]
+                        subj, word, block, token, filename = frame_path.split('/')
+                    except ValueError:
+                        print("Your directories are not structured right. Readjust!")
+                        sys.exit(2)
+                elif len(glob_finder) > 1:
+                    print("Could not find file with desired index {}".format(idx_int))
+                    sys.exit(2)
+                elif len(glob_finder) < 1:
+                    print("Too many files with desired index {}".format(idx_int))
+                    sys.exit(2)
+                
+                # get more metadata from filename
+                filename = os.path.splitext(filename)[0]
+                filename = re.sub("__", "_", filename)
+                subj_dupl, timestamp, idx_dupl = filename.split("_")
+                
+                # convertingom RGB to grayscale (one-channel):
+                print(frame_path)
+                inframe = np.asarray(Image.open(frame_path).convert("L")) # converted to uint8:
+                rawdata = inframe.astype(np.uint8)
+                # TODO filter and ROI somehow (use SRAD) (maybe ROI will have to be skipped?)
+                # TODO downsample size of images?
+                series_frames.append(rawdata)
+            
+            #series = np.hstack(series_frames)
+            
+            if data is None:
+                data = np.expand_dims(series_frames, axis=0)
+            else:
+                data = np.concatenate([data, np.expand_dims(series_frames, axis=0)])
+                
+            recs.append(
+                OrderedDict([
+                ('filename', timestamp),
+                ('subject', subj),
+                ('stim', word),
+                ('token', token),
+                ('index', idx_int),
+                ('sha1', sha1(series_frames[0].ravel()).hexdigest()), # tuple error is thrown here.
+                ('sha1_dtype', series_frames[0].dtype)
+                ])
+            )
+            
+            # convert metadata to a DataFrame
+            md = pd.DataFrame.from_records(recs, columns=recs[0].keys())
 
-	# will get subject, word, token, etc out of four-level nested dir
-	# that seems to be typical of AAA output
-	name = os.path.split(frame)[1]
-	basename = os.path.splitext(name)[0]
-	idx = basename.split('_')[-1]
-	subj, word, token = os.path.dirname(frame).split('/')
-	print(subj, word, token, basename, idx)
+            # make sure there is one metadata row for each ndarray in the pickle
+            assert(len(md) == data.shape[0])
 
-	# get ndarray from image file. issue is probably here. Unconverted RGB:
-	inframe = np.asarray(Image.open(frame)) 
-	# converted from RGB to grayscale (one-channel):
-	inframe = np.asarray(Image.open(frame).convert("L")) 
-	# converted to uint8:
-	rawdata = inframe.astype(np.uint8)
+            # compare checksums
+            assert(md.loc[0, 'sha1'] == sha1(data[0][0].ravel()).hexdigest())
+            assert(md.loc[len(md)-1,'sha1'] == sha1(data[-1][0].ravel()).hexdigest())
 
-	recs.append(
-		OrderedDict([
-			('filename', basename), 
-			('subject', subj),
-			('stim', word),
-			('token', token),
-			('index', idx),
-			('sha1', sha1(rawdata.ravel()).hexdigest()), # tuple error is thrown here.
-			('sha1_dtype', rawdata.dtype)
-		])
-	)
-
-	# add frame ndarray to frames list
-	if data is None:
-		data = np.expand_dims(rawdata, axis=0)
-	else:
-		data = np.concatenate([data, np.expand_dims(rawdata, axis=0)])
-
-# convert metadata to a DataFrame
-md = pd.DataFrame.from_records(recs, columns=recs[0].keys())
-
-# make sure there is one metadata row for each ndarray in the pickle
-assert(len(md) == data.shape[0])
-
-# compare checksums
-assert(md.loc[0, 'sha1'] == sha1(data[0].ravel()).hexdigest())
-assert(md.loc[len(md)-1,'sha1'] == sha1(data[-1].ravel()).hexdigest())
-
-np.save(frames_out, data)
-md.to_pickle(metadata_out)
+            np.save(frames_out, data)
+            md.to_pickle(metadata_out)
---
+++ scripts/dim-reduction/punjabi-series-process-cache.py
@@ -0,0 +1,157 @@
+import os, sys, glob, re
+import argparse
+import numpy as np 
+import pandas as pd
+#from ultratils.pysonix.scanconvert import Converter
+from hashlib import sha1
+#from sklearn.decomposition import PCA
+from scipy.ndimage import median_filter
+#from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import matplotlib.pyplot as plt
+from imgphon.imgphon import ultrasound as us
+
+# read in args
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help="Experiment directory containing all subjects")
+args = parser.parse_args()
+
+# check for appropriate directory
+expdir = args.directory
+try:
+    assert os.path.exists(args.directory)
+except AssertionError:
+    # TODO raise exception
+    print("\tDirectory provided doesn't exist")
+    parser.print_help()
+    sys.exit(2)
+
+yes_no = ["Y", "N"]
+
+subject = "P" + re.sub("[^0-9]", "", expdir)
+
+data_in = os.path.join(expdir,"frames.npy")
+data = np.load(data_in)
+metadata_in = os.path.join(expdir,'frames_metadata.pickle')
+md = pd.read_pickle(metadata_in)
+
+# some sanity checks on data checksums
+assert(len(md) == data.shape[0]) # make sure one md row for each frame
+assert(md.loc[0, 'sha1'] == sha1(data[0][0].ravel()).hexdigest()) # checksums
+assert(md.loc[len(md)-1,'sha1'] == sha1(data[-1][0].ravel()).hexdigest())
+
+# TODO how to get the mean frame object for a 4D array?
+# make temp flattened array and take mean of that
+data_for_mean = data.reshape([
+    data.shape[0] * data.shape[1], 
+    data.shape[2],
+    data.shape[3]
+    ])
+#print(data_for_mean.shape)
+mean_frame = data_for_mean.mean(axis=0)
+#conv_mean = np.flipud(conv.convert(np.flipud(mean_frame)))
+plt.title("Mean frame, Spkr {:}".format(subject))
+plt.imshow(mean_frame, cmap="Greys_r")
+file_ending_mean = "subj{:}_mean.pdf".format(subject)
+savepath_mean = os.path.join(expdir,file_ending_mean)
+plt.savefig(savepath_mean)
+roi_upper = 325
+roi_lower = 200
+roi_left = 200
+roi_right = 400
+
+# TODO give mask converted shape
+while True:
+    mask = us.roi(mean_frame, 
+        upper=roi_upper, 
+        lower=roi_lower,
+        left=roi_left,
+        right=roi_right)
+    masked_mean = mean_frame * mask
+    #conv_masked = np.flipud(conv.convert(np.flipud(masked_mean)))
+    plt.title("Mean frame and RoI, {:}".format(subject))
+    plt.imshow(masked_mean, cmap="Greys_r")
+    file_ending_roi = "subj{:}_roi.pdf".format(subject)
+    savepath_roi = os.path.join(expdir,file_ending_roi)
+    plt.savefig(savepath_roi)
+    good_roi = input("Inspect {:}. Good RoI? (Y/N) ".format(savepath_roi))
+
+    # TODO improve typo handling
+    if good_roi.upper() in yes_no:
+        if good_roi.upper() == "Y":
+            break
+        else:
+            roi_upper = int(input("Please provide a new upper bound for RoI (currently {:}): ".format(roi_upper)))
+            roi_lower = int(input("Please provide a new lower bound for RoI (currently {:}): ".format(roi_lower)))
+            roi_left = int(input("Please provide a new left bound for RoI (currently {:}): ".format(roi_left)))
+            roi_right = int(input("Please provide a new right bound for RoI (currently {:}): ".format(roi_right)))
+    else:
+        print("Typo, try again ...")
+
+adj_radius = int(data[0][0].shape[0]/50) # short side of single frame /50, for median filter
+
+print("Preprocessing data for PCA ...")
+
+in_series = data[0] # array
+out_frames_samp = []
+padding = 5 # number of pixels to tack on at edges to visually divide frames
+for frame in in_series:
+    crop_samp = frame[roi_lower:roi_upper, roi_left:roi_right]
+    sradd_samp = us.srad(crop_samp)
+    clean_samp = us.clean_frame(sradd_samp, median_radius=adj_radius)
+#    masked_samp = clean_samp * mask # using mask defined above
+    rescaled_samp = clean_samp * 255
+    sample_frame = rescaled_samp.astype(np.uint8)
+    sample_frame = np.pad(sample_frame, padding, 'constant')
+    out_frames_samp.append(sample_frame)
+
+out_series_samp = np.hstack(out_frames_samp)
+plt.title("Sample frames series, Spkr {:}".format(subject))
+plt.imshow(out_series_samp, cmap="Greys_r")
+file_ending_sample = "subj{:}_sample.pdf".format(subject)
+savepath_sample = os.path.join(expdir,file_ending_sample)
+plt.savefig(savepath_sample)
+print("Please check sample series at {}!".format(savepath_sample))
+
+# preallocate ultrasound frame array for PCA
+out_serieses = np.empty([data.shape[0]] + list(out_series_samp.shape)) * np.nan
+out_serieses = out_serieses.astype('uint8')
+filt_hds = []
+total = out_serieses.shape[0]
+
+frames_out = "frames_proc.npy"
+metadata_out = "frames_proc_metadata.pickle"
+
+# TODO loop index issues (get IndexError "out of range" at item 5)
+for idx,series in enumerate(data):
+    out_frames = []
+    for frame in series:
+        crop = frame[roi_lower:roi_upper, roi_left:roi_right]
+        sradd = us.srad(crop) 
+        clean = us.clean_frame(sradd, median_radius=adj_radius)
+        rescaled = clean * 255
+        out_frame = rescaled.astype(np.uint8)
+        out_frame = np.pad(out_frame, padding, 'constant')
+        out_frames.append(out_frame)
+
+    out_series = np.hstack(out_frames)
+    out_serieses[idx,:,:] = out_series
+    # new sha1 hex: filtered, conv to np.uint8
+    filt_hds.append(sha1(out_serieses[idx].ravel()).hexdigest()) 
+    print("\tAdded series {} of {}".format(idx+1,total))
+
+# add new sha1 hex as a column in the df
+md = md.assign(sha1_filt=pd.Series(filt_hds, index=md.index))
+
+# make sure there is one metadata row for each image frame
+assert(len(md) == out_serieses.shape[0])
+
+# for debugging
+# pca_md.to_csv(os.path.join(root,d,"test.csv"))
+
+# compare checksums
+assert(md.loc[0, 'sha1_filt'] == sha1(out_serieses[0].ravel()).hexdigest())
+assert(md.loc[len(md)-1,'sha1_filt'] == sha1(out_serieses[-1].ravel()).hexdigest())
+        
+ # output
+np.save(os.path.join(expdir,frames_out), out_serieses)
+md.to_pickle(os.path.join(expdir,metadata_out))
