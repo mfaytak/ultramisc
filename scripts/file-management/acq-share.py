d0c6fc9e49e61557f596bc3fc59fe4c4f1cbb913
mfaytak <faytak@ucla.edu>
2019-08-01 23:49:08 -0700

    Flat dir feature in acq-share

--- scripts/file-management/acq-share.py
+++ scripts/file-management/acq-share.py
@@ -1,12 +1,17 @@
 
 '''
-acq-share.py: divide ultrasound subdirectories into approximately
+acq-share.py: by Matt Faytak / Jennifer Kuo
+Divide ultrasound subdirectories into approximately
   equal shares for processing by multiple workers. 
-  Usage: python ultra-splitter.py [expdir] [shares]
-         expdir: the experiment folder containing the acquisition 
-           subdirectories to be divided.
-         shares: the number of shares to divide the acquisitions
-           into.
+  Usage: python acq-share.py [expdir] [shares] [--flat] [--random]
+		 expdir: the experiment folder containing the acquisition 
+		   subdirectories to be divided.
+		 shares: the number of shares to divide the acquisitions
+		   into.
+		 --flat: if specified, assume the directory structure is
+		   flat (all files in one folder, which is expdir).
+		 --random: if specified, take randomly selected subsets
+		   (otherwise, contiguous parts of sorted list are taken).
 '''
 
 import argparse
@@ -28,9 +33,17 @@ parser.add_argument("expdir",
 					)
 parser.add_argument("shares",
 					type=int,
-                    help="Number of shares to divide the data into"
-                    )
-# TODO option for flat directory structure?
+					help="Number of shares to divide the data into"
+					)
+parser.add_argument("--flat","-f",
+					action='store_true',
+					help="If specified, expect a flat directory structure within expdir"
+					)
+parser.add_argument("--random","-r",
+					action='store_true',
+					help="If specified, divide pseudo-randomly into subsets; \
+						  otherwise use contiguous parts of sorted file list"
+					)
 args = parser.parse_args()
 
 try:
@@ -41,19 +54,51 @@ except IndexError:
 	ArgumentParser.print_help
 	sys.exit(2)
 
-tg_glob_exp = os.path.join(expdir, "*", "*.ch1.TextGrid")
-
-coders = args.shares # TODO make arguments
+coders = args.shares
 counter = 0
 
-# TODO convert to "flat structure"
-# TODO have "flat structure option"
-for part in split(glob.glob(tg_glob_exp), coders):
-	print("listy list")
-	counter += 1
-	out = os.path.join(expdir,"_out{}".format(counter))
-	for tg in part:
-		parent = os.path.dirname(tg)
-		print(parent)
-		out_dest = os.path.join(out, os.path.split(parent)[-1])
-		shutil.copytree(parent, out_dest)
+if args.flat:
+
+	# look through directory for all files matching basename;
+	tg_glob = glob.glob(os.path.join(expdir, "*.ch1.TextGrid"))
+
+	if not args.random:
+		tg_glob.sort()
+
+	for part in split(tg_glob, coders):
+		# name output dir according to coder number
+		counter += 1
+		out = os.path.join(expdir,"_share{}".format(counter))
+		os.makedirs(out)
+
+		# go through and copy all files matching TG basename
+		for tg in part:
+			basename = os.path.split(tg)[-1].split('.')[0]
+			matching_file_exp = os.path.join(expdir,str(basename + "*"))
+			
+			for f in glob.glob(matching_file_exp):
+				out_dest = os.path.join(out,os.path.split(f)[-1])
+				shutil.copy(f,out_dest)
+
+else:
+
+	# look into an additional layer of subdirectories, 
+	# and copy contents of each subdir
+	# TODO this also captures flattened dirs and all their contents.
+	tg_glob = glob.glob(os.path.join(expdir, "*", "*.ch1.TextGrid"))
+
+	if not args.random:
+		tg_glob.sort()
+
+	if len(tg_glob) == 0:
+		print("WARNING: No TextGrids found. Are you supposed to be using --flat?")
+
+	for part in split(tg_glob, coders):
+		counter += 1
+		out = os.path.join(expdir,"_out{}".format(counter))
+		
+		for tg in part:
+			parent = os.path.dirname(tg)
+			#print(parent)
+			out_dest = os.path.join(out, os.path.split(parent)[-1])
+			shutil.copytree(parent, out_dest)
